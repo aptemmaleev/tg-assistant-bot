8a7caf875b96ba3890d397f424226ecc22d947df
Aptem <maleev2002@yandex.ru>
2022-11-06 22:31:10 +0300

    Added module loader

--- bot.py
+++ bot.py
@@ -1,33 +1,32 @@
+import os
+import importlib
 from config import config
-from utils.db import PostgresDatabase
-from aiogram import Bot, Dispatcher, executor, types
+from modules.utils.db import PostgresDatabase
+from aiogram import Bot, Dispatcher, executor
 
-# Initialize
+from filters.role import setup_permissions
+
+# Initialize database
 PostgresDatabase(host=config.database.host,
                  port=config.database.port,
                  database=config.database.database,
                  user=config.database.user,
                  password=config.database.password)
 
+# Initialize telegram bot
 bot = Bot(token=config.token)
 dp = Dispatcher(bot)
 
-# Start command handler
-@dp.message_handler(commands=["start", "help"])
-async def welcome_message(message: types.Message):
-    # Get user from database
-    db_user = PostgresDatabase.get_user(message.from_id)
-    if (db_user == None):
-        db_user = PostgresDatabase.add_user(message.from_id)
-    # User is teacher or not
-    answer = ""
-    if (db_user[2]):
-        answer += "*Вы уже получили доступ к боту\\!* \n"
-        answer += "Список команд: /help"
-    else:
-        answer += f"*Добро пожаловать, {message.from_user.first_name}*\\! \n"
-        answer += "Чтобы начать пользоваться ботом необходимо пройти верификацию\\."
-    await message.answer(answer, parse_mode="MarkdownV2")
+# Initialize filters
+setup_permissions(dp)
+
+# Loading modules
+tree = os.listdir('modules')
+for module in tree:
+    if (not module.startswith('__') and not module.startswith('utils')):
+        path = 'modules.' + module[:-3]
+        mod = importlib.import_module(path)
+        mod.setup(dp, bot)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
