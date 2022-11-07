d6e00cd53bd83f85b6f360ef4d6cd159281b23b4
Aptem <maleev2002@yandex.ru>
2022-11-08 00:52:34 +0300

    Moved files

--- modules/utils/buttons.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-# Start verification
-start_verification_button = InlineKeyboardButton(text='Начать верификацию',
-                                                 callback_data='start_verification_button')
-start_verification_keyboard = InlineKeyboardMarkup()
-start_verification_keyboard.add(start_verification_button)
