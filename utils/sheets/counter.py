80d03d824037c18c29d0ddedf1eddc2352b26d27
Aptem <maleev2002@yandex.ru>
2022-11-18 00:20:28 +0300

    Relocating files

--- utils/sheets/counter.py
+++
@@ -1,7 +0,0 @@
-def add_char(letter: str, n: int):
-    c = ord(letter) - 65 + n
-    if (c > 25):
-        return(chr(c // 26 + 65 - 1) + chr(c % 26 + 65))
-    else:
-        return(chr(c % 26 + 65))
-    
