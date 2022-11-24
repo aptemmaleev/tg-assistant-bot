9cee462e824270f76e53d00d0e90a1735482bf25
Aptem <maleev2002@yandex.ru>
2022-11-24 18:27:30 +0300

    New keyboards

--- modules/elements/keyboards.py
+++ modules/elements/keyboards.py
@@ -37,7 +37,7 @@ class AddGoogleSheetsInlineKeyboard(InlineKeyboardMarkup):
         super().__init__(row_width)
         self.add(InlineKeyboardButton('Далее', callback_data=f'groups_add_sheets'))
         
-# Delete group inline keyboard
+# Select group inline keyboard
 class SelectedGroupInlineKeyboard(InlineKeyboardMarkup):
     """SelectedGroupInlineKeyboard
 
@@ -50,3 +50,30 @@ class SelectedGroupInlineKeyboard(InlineKeyboardMarkup):
         self.add(InlineKeyboardButton('ACMP', callback_data=f'groups_acmp_{group_index}'))
         self.add(InlineKeyboardButton('Назад', callback_data=f'return_to_groups'))
 
+# OnlineGDB Menu
+class OnlineGdbInlineKeyboard(InlineKeyboardMarkup):
+    """OnlineGdbInlineKeyboard
+
+    callback_data:
+        "onlinegdb_{group_index}"
+    """
+    def __init__(self, group_index, row_width=1):
+        super().__init__(row_width)
+        self.add(InlineKeyboardButton('Проверить задачу', callback_data=f'onlinegdb_review_one_{group_index}'))
+        self.add(InlineKeyboardButton('Выставить оценки', callback_data=f'onlinegdb_grade_one_{group_index}'))
+        self.add(InlineKeyboardButton('Проверить все', callback_data=f'onlinegdb_review_all_{group_index}'))
+        self.add(InlineKeyboardButton('Выставить все', callback_data=f'onlinegdb_grade_all_{group_index}'))
+        self.add(InlineKeyboardButton('Назад', callback_data=f'return_to_groups'))
+
+
+# Acmp Menu
+class AcmpInlineKeyboard(InlineKeyboardMarkup):
+    """AcmpInlineKeyboard
+
+    callback_data:
+        "onlinegdb_{group_index}"
+    """
+    def __init__(self, group_index, row_width=1):
+        super().__init__(row_width)
+        self.add(InlineKeyboardButton('Выгрузить выполнивших', callback_data=f'acmp_review_one_{group_index}'))
+        self.add(InlineKeyboardButton('Назад', callback_data=f'return_to_groups'))
